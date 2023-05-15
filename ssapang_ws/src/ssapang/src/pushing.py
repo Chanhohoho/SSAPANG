a119c980e76a969ecf4d0da9ddc0eecd1f68fc84
cksgh0109 <cksgh0109@naver.com>
2023-05-15 17:58:58 +0000

    🚀 [S08P31A409-345] Bot / push_robot 노드 연결

--- ssapang_ws/src/ssapang/src/pushing.py
+++ ssapang_ws/src/ssapang/src/pushing.py
@@ -2,24 +2,35 @@
 import rospy
 from push_robot_node import push_robot_node
 from ssapang.msg import str
-import os,sys,time
+from std_msgs.msg import Float64
+import time
 
 def pushing(msg):
+
     burger_node = msg.data
     push_code = push_robot_node.get(burger_node)
-    # print(push_code)
     parameter = push_code.split('-')
-    # print(parameter[0],parameter[1])
-    print(f"rostopic pub -1 /rrbot{parameter[0]}/joint1_position_controller/command std_msgs/Float64 \"data: 1.5\"")
-    # os.system('gazebo')
+    
+    data = Float64()
+    data.data = 1.5
+    pub[int(parameter[0])].publish(data)
+
     for i in range (3):
         time.sleep(1)
-        print(i+1)
-    print(f"rostopic pub -1 /rrbot{parameter[1]}/joint1_position_controller/command std_msgs/Float64 \"data: 1.5\"")
 
-# pushing('BP0303')
+    data.data = 0
+    pub[int(parameter[0])].publish(data)
 
 if __name__ == '__main__':
+
     rospy.init_node('pushing')
+
+    n = 18
+    pub = [None]*(n+1)
+    for i in range(1,n+1):
+        topic = '/rrbot{num}/joint2_position_controller/command'
+        pub[i] = rospy.Publisher(topic.format(num=i), Float64, queue_size=1)
+
     sub=rospy.Subscriber('picking', str, pushing)
+
     rospy.spin()
